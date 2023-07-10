8a9cf269f4b4c53ecfdae97473d870ffbfb41b82
Eunjin <eunjin.kang22@gmail.com>
2023-07-11 00:11:20 +0900

    ui-layout edit

--- view.py
+++ view.py
@@ -1,53 +1,51 @@
 import sys
-
 from PySide2.QtWidgets import *
 
 
 class RenameView(QWidget):
-    form_group_box = None
-    layout = None
-    row_counter = None
-
     def __init__(self):
         super().__init__()
-
+        
         self.row_counter = 0
         self.dir_path = None
         self.old_list = []
         self.new_list = []
         self.create_groupbox()
+        self.setupUI()
 
-        layout_side = QHBoxLayout()
-        self.line_path = QLineEdit()
+    def setupUI(self):
+        layout_left_in = QHBoxLayout()
+        layout_left = QVBoxLayout()
+        layout_right_in = QHBoxLayout()
+        layout_right = QVBoxLayout()
+        layout_main = QHBoxLayout()
+
+        self.line_path = QLineEdit("Path")
+        self.line_name = QLineEdit("Current File Name")
         self.btn_browse = QPushButton("Browse")
-        self.btn_browse.clicked.connect(self.btn_browse_event)
-        layout_side.addWidget(self.line_path)
-        layout_side.addWidget(self.btn_browse)
 
         self.btn_add = QPushButton("+")
         self.btn_add.clicked.connect(self._insert_row_lineedit)
-
+        self.btn_sub.clicked.connect(self.btn_sub_event)
         self.btn_rename = QPushButton("Rename")
-        self.btn_rename.clicked.connect(self.btn_rename_event)
 
-        layout_main = QVBoxLayout()
-        layout_side.addLayout(layout_main)
+        layout_left_in.addWidget(self.line_path)
+        layout_left_in.addWidget(self.btn_browse)
+        layout_left.addLayout(layout_left_in)
+        layout_left.addWidget(self.line_name)
 
-        layout_main.addWidget(self.form_group_box)
-        layout_main.addWidget(self.btn_add)
-        layout_main.addWidget(self.btn_rename)
+        layout_right.addWidget(self.form_group_box)
+        layout_right_in.addWidget(self.btn_add)
+        layout_right.addLayout(layout_right_in)
+        layout_right.addWidget(self.btn_rename)
 
-        self.setLayout(layout_side)
+        layout_main.addLayout(layout_left)
+        layout_main.addLayout(layout_right)
+
         self.setLayout(layout_main)
         self.setWindowTitle("Renamer")
 
         self.show()
-
-    def btn_browse_event(self):
-        print("Select Directory")
-
-    def btn_rename_event(self):
-        print('Change name')
 
     def _insert_row_lineedit(self):
         line_old = QLineEdit()
@@ -55,9 +53,11 @@ class RenameView(QWidget):
         label = QLabel(" -> ")
         line_new = QLineEdit()
         line_new.setPlaceholderText("new name")
+        self.btn_sub = QPushButton("-")
         self.layout.addWidget(line_old, self.row_counter, 0)
         self.layout.addWidget(label, self.row_counter, 1)
         self.layout.addWidget(line_new, self.row_counter, 2)
+        self.layout.addWidget(self.btn_sub, self.row_counter, 3)
 
         self.old_list.append(line_old)
         self.new_list.append(line_new)
@@ -70,9 +70,12 @@ class RenameView(QWidget):
         self._insert_row_lineedit()
         self.form_group_box.setLayout(self.layout)
 
+    def btn_sub_event(self):
+        "remove QLineEdit"
+        pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     rv = RenameView()
-    sys.exit(app.exec_())
-
+    app.exec_()
